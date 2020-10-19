a9f15bb6a0c285ca38c4476e697327fd748bac38
Nidhi S. Kushwaha <nidhi.knit09@gmail.com>
2020-10-19 12:46:35 +0530

    FInal commit

---
+++ src/test/java/com/example/jungleegames/spring/cloudgateway/poc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jungleegames.spring.cloudgateway.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
